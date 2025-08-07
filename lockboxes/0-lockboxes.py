9279dfd5a1e03a060547135614401ea8e5aab20c
KamalMusayev <kmusayev038@gmail.com>
2025-08-07 14:25:40 +0000

    0-lockboxes.py

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Function is starting"""
+
+
+def canUnlockAll(boxes):
+    """
+    Inside of Function
+    """
+    n = len(boxes)
+    opened = set()
+    stack = [0]
+
+    while stack:
+        current = stack.pop()
+        if current not in opened:
+            opened.add(current)
+            for key in boxes[current]:
+                if key < n:
+                    stack.append(key)
+
+    return len(opened) == n
